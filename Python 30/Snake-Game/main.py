7022a2e98f18bf7adba7602b1b2732d4fb6a55a6
NASIM <nosytub@gmail.com>
2024-11-24 09:11:41 +0530

    .

---
+++ Python 30/Snake-Game/main.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+from snake.game import Game, GameConf, GameMode
+
+greedy="GreedySolver"
+hamilton="HamiltonSolver"
+
+normal=GameMode.NORMAL
+
+conf=GameConf()
+conf.solver_name=hamilton
+conf.mode=normal
+print("Solver:%s"%(conf.solver_name))
+print("Mode:%s"%(conf.mode))
+Game(conf).run()
